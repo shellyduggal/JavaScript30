b99d8571ea07e6a615b77e7c5dea038a1090d9bc
Shelly Duggal <duggalsf@gmail.com>
2017-01-04 14:37:19 -0600

    associate keyboard event with html elements

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-function sound(event) {
-	var letter = event.keyCode;
-	//?Need to figure out how to point to audio but not .key with data-key
-	document.querySelector("[data-key='letter']").play();
+function play(event) {
+	//associate pressed key (event) with html elements
+	var sound = document.getElementById(event.keyCode);
+	var letter = document.querySelector(`.key[data-key="${event.keyCode}"]`); 
+	//play the sound
+	sound.play();
+	//highlight the letter on-screen
+	letter.classList.add('playing');
+	//reset sound
+	sound.currentTime = 0;
+	//reset highlight
+	letter.addEventListener('transitionend', function() {
+		letter.classList.remove('playing');
+	});
+	
 }
 
-window.addEventListener("keypress",sound);
+window.addEventListener("keydown",play);
+
+
+
+//REQS: 
+//1. SOUNDS 
+//2. HIGHLIGHT 
+//3. RESET
+//4. REFINE
